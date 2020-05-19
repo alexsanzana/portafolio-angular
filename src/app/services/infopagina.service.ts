21646e1b1acbfcebac153d85e35dea55ee97c1e6
Alex Sanzana <alexsanzana1@gmail.com>
2020-05-19 19:51:25 -0400

    final seccion 5

--- src/app/services/infopagina.service.ts
+++ src/app/services/infopagina.service.ts
@@ -9,15 +9,27 @@ export class InfopaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any;
 
   constructor( private http: HttpClient ) {
+    this.caragarInfo();
+    this.cargarEquipo();
+  }
 
-    this.http.get('assets/data/data-pagina.json')
+  private caragarInfo() {
+     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
       });
-
   }
+
+  private cargarEquipo() {
+     this.http.get('https://angular-html-1096b.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
+  }
+
 }
